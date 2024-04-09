44f60e3b62185afe01db82dfdf0b6dfc6e5d25a9
Danie Silva <dnlsilva191@gmail.com>
2024-04-09 08:09:00 -0300

    created ver. 0.10

---
+++ src/js/home.js
@@ -0,0 +1,22 @@
+
+const audio = new Audio("../assets/theme.mp3")
+const audioPlay = new Audio("../assets/play.mp3")
+const buttonPlay = document.querySelector(".btn-play")
+const buttonAudio = document.querySelector(".btn-aud")
+ 
+    audio.play()
+
+    buttonPlay.addEventListener("click", () => {
+        score.innerText = "00"
+        menu.style.display = "none"
+        canvas.style.filter = "none"
+        audioPlay.play()
+        snake = [initialPosition]})
+
+        buttonAudio.addEventListener("click", () => {
+            if( audio.muted ){
+                audio.muted = false;
+            }else{
+                audio.muted = true;
+            }
+        })
